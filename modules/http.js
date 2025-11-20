f027ddd5c6487f5cebbe925d0c85355577989c50
KritikaSapkotaa <krisapkota2@gmail.com>
2025-11-20 23:15:10 +0545

    Asynchronous programming

--- modules/http.js
+++ modules/http.js
@@ -2,17 +2,22 @@ import http from "http";
 
 const app = http.createServer((request, response) => {
     console.log(request.method);
+    console.log(request.url);
 
-    const data = {
-        version: "0.1.0",
-        name: "MERN Stack API",
-        status: "Ok",
-    };
+    if(request.url === "/") {
+    response.writeHead(200, { "context-type": "text/html" });
+    response.end("<h1>Home page</h1>");
+    } else if (request.url === "/about") {
+        response.writeHead(200, {"content-type": "text/html"})
+        response.end("<h1>About page</h1>");
+    }else{
+        response.writeHead(404, {"content-type": "text/html"});
+        response.end("<h1>Page not found</h1>");
+    }
 
-    response.writeHead(200, { "context-type": "application/json" });
-    response.end(JSON.stringify(data));
+    
 });
 
 app.listen(5000, () => {
-    console.log("Server runninf at port 5000...");
+    console.log("Server running at port 5000...");
 });
